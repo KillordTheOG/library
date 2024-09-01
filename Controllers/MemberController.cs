322b78ffe1f3b249e224f7f76cfbf14a8b20153a
Gușă David-Emil <85167989+KillordTheOG@users.noreply.github.com>
2024-09-01 22:44:48 +0300

    Added membercontroller with create and index

---
+++ Controllers/MemberController.cs
@@ -0,0 +1,103 @@
+﻿using Library.Data;
+using Library.Models;
+using Library.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    public class MemberController : Controller
+    {
+        private MemberRepository memberRepository;
+
+        public MemberController(ApplicationDbContext dbContext)
+        {
+            memberRepository = new MemberRepository(dbContext);
+        }
+
+        // GET: MemberController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: MemberController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MemberController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MemberController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                MemberModel model = new MemberModel();
+
+                var task = TryUpdateModelAsync(model);
+                task.Wait();
+
+                if (task.Result)
+                {
+                    memberRepository.InsertMember(model);
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MemberController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MemberController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MemberController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MemberController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Models/MemberModel.cs
+++ Models/MemberModel.cs
@@ -1,11 +1,21 @@
-﻿namespace Library.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
 {
     public class MemberModel
     {
         public Guid Idmember { get; set; }
+
+        [Required]
         public string Name { get; set; } = null!;
+
+        [Required]
         public string Adress { get; set; } = null!;
+
+        [Required]
         public string Email { get; set; } = null!;
+
+        [Required]
         public string Phone { get; set; } = null!;
     }
 }
